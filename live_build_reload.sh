a57f51710e44eb72f5e6c61efc3266516c30246d
Matteo Longhi <mlonghi@labs.it>
2025-02-24 12:04:24 +0100

    fixed live build script

--- live_build_reload.sh
+++ live_build_reload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-TARGET_FILES="$(realpath main.tex) graphs Makefile"
-inotifywait --monitor --recursive --event modify --format %w $TARGET_FILES | while read file; do
-  echo "$path" "$action" "$file"
+TARGET_FILES="main.tex graphs Makefile"
+# event ignored captured cause vim replace file with swapfile and does not generate modify event
+while true; do
+  inotifywait --recursive --event modify --event close_write --event ignored --format "%w %e" $TARGET_FILES | while read file event; do
   make build
+done
 done
