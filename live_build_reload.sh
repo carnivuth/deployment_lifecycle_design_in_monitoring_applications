e0b79e9cd06043d7fcc339580030e9fe5d1c97a3
carnivuth <matti200042@gmail.com>
2025-03-01 13:05:39 +0100

    limited build only to modified files

--- live_build_reload.sh
+++ live_build_reload.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-TARGET_FILES="main.tex graphs Makefile"
+TARGET_FILES="main.tex graphs/*"
+BUILDDIR="./build"
 # event ignored captured cause vim replace file with swapfile and does not generate modify event
 while true; do
-  inotifywait --recursive --event modify --event close_write --event ignored --format "%w %e" $TARGET_FILES | while read file event; do
-  make build
+  inotifywait --event modify --event close_write --event ignored --format "%w %e" $TARGET_FILES | while read file event; do
+
+  filename=$(basename -- "$file")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+
+  case "$extension" in
+    mmd)
+      # limit build to modified files only
+      make "$BUILDDIR/$filename.png"
+      make "main.pdf"
+      ;;
+    tex)
+      make "main.pdf"
+      ;;
+    *)
+      make build
+  esac
 done
 done
